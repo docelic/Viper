89223ee515061442dfd478bedb7dad658c531663
Davor Ocelic <docelic@spinlocksolutions.com>
2009-09-25 18:07:48 +0200

    Tweaks

--- etc/puppet/manifests/site.pp
+++ etc/puppet/manifests/site.pp
@@ -1,6 +1,12 @@
-filebucket { server:
-	server => "puppet"
-}
+# site.pp
 
-# don't distribute version control metadata
-#File { ignore => ['.svn', '.git', 'CVS' ] }
+filebucket { main: server => "puppet" }
+
+# Global defaults
+
+File { backup => main }
+# Don't distribute version control metadata
+File { ignore => [ '.svn', '.git', 'CVS' ] }
+
+Exec { path => "/usr/bin:/usr/sbin/:/bin:/sbin" }
+
