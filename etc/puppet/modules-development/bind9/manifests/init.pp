e3dc1f509d9f46e39818b0a1ee9c89dd81857a16
root <root@inst.spinlock.hr>
2009-09-26 23:12:26 +0200

    Bind9 updates

--- etc/puppet/modules-development/bind9/manifests/init.pp
+++ etc/puppet/modules-development/bind9/manifests/init.pp
@@ -1,37 +1,54 @@
 class bind9 {
 
 	class config {
-#		$bind_dir = f("/etc/bind")
-#
-#		file { $bind_conf:
-#				ensure  => file,
-#				mode    => 0644,
-#				owner   => root,
-#				group   => root,
-#				content => template('bind/bind.conf.erb'),
-#				before  => Package[$bind],
-#		}
+		$bind9_conf = lookup("file", "/etc/bind/named.conf.local")
+		$bind9_opts = lookup("file", "/etc/bind/named.conf.options")
+
+		file {
+			$bind9_conf:
+				ensure  => file,
+				mode    => 0644,
+				owner   => root,
+				group   => bind,
+				content => template('bind9/named.conf.local.erb'),
+				before  => Package[$bind9];
+			$bind9_opts:
+				ensure  => file,
+				mode    => 0644,
+				owner   => root,
+				group   => bind,
+				content => template('bind9/named.conf.options.erb'),
+				before  => Package[$bind9];
+		}
 
 		class disabled {
-			$bind_dir = f("/etc/bind")
+			$bind9_dir = lookup("file", "/etc/bind/")
 
-			file { $bind_dir:
+			file { $bind9_dir:
 				ensure => absent,
 			}
 		}
 	}
 
 	class package {
-		$bind= [ p("bind9"), p("bind9-host"), p("bind9utils") ]
+		$bind9 = [
+			lookup("package", "bind9"),
+			lookup("package", "bind9-host"),
+			lookup("package", "bind9utils")
+		]
 
-		package { $bind:
+		package { $bind9:
 				ensure => latest,
 		}
 
 		class disabled {
-			$bind= [ p("bind9"), p("bind9-host"), p("bind9utils") ]
+			$bind9 = [
+				lookup("package", "bind9"),
+				lookup("package", "bind9-host"),
+				lookup("package", "bind9utils")
+			]
 
-			package { $bind:
+			package { $bind9:
 				ensure => purged,
 			}
 		}
