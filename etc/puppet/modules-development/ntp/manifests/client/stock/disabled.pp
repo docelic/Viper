b4db26fdade3799d29abbc78146237d4bbe26174
Davor Ocelic <docelic@spinlocksolutions.com>
2009-09-26 20:11:46 +0200

    NTP module updates

--- etc/puppet/modules-development/ntp/manifests/client/stock/disabled.pp
+++
@@ -1,4 +0,0 @@
-class ntp::client::stock::disabled {
-	include ntp::config::disabled
-	include ntp::package::disabled
-}
--- etc/puppet/modules-development/ntp/manifests/server/stock/disabled.pp
+++
@@ -1,4 +0,0 @@
-class ntp::server::stock::disabled {
-	include ntp::config::disabled
-	include ntp::package::disabled
-}
